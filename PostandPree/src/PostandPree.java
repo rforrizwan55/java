af42744a0ba1239d24b9e97da48c6f92b41eb6b6
abdulraqeeb <abdulraqeeb@10.100.42.174>
2016-07-07 18:07:37 -0500

    initial

---
+++ PostandPree/src/PostandPree.java
@@ -0,0 +1,15 @@
+
+public class PostandPree {
+
+	public static void main(String[] args) {
+		int i=5,j;
+		
+		j=i++; //post
+		System.out.println(j+" : "+i);
+		j=++i; // pre
+		
+		System.out.println(j+" : "+i);
+
+	}
+
+}
